892355cf86d633ed738ed39b78955c3e37ac4fd7
Antonio <antonioboksic1603@gmail.com>
2023-05-11 23:10:37 +0200

    restituisco all'utente il proprio punteggio e restituisco posizione dei numeri indovinati

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 
 // crea un array di elementi che si riferiscono ai 5 numeri
 let elGameNumbers = document.querySelectorAll(".numero")
-console.log(elGameNumbers)
+console.log(elGameNumbers);
+
+// crea un array dove pushare i valori degli elementi HTML
+let gameNumbers = [];
+
+for ( i = 0 ; i < elGameNumbers.length ; i++) {
+    console.log(parseInt(elGameNumbers[i].innerHTML))
+    gameNumbers.push(parseInt(elGameNumbers[i].innerHTML))
+}
+console.log(gameNumbers);
 
 //crea un array con numeri scelti dall'utente
-let UserNumbers = []
+let userNumbers = [];
+
+//creo una variabile dove inserire tentativi corretti
+let points = 0;
+
+//creo un array dove pushare dentro posizioni numeri indovinati
+positionRightNumbers = [];
 
 // dopo 5000ms attiva funzione eliminateNumbers e poi askNumbers
 setTimeout(eliminateNumbers,5000)
@@ -12,7 +27,22 @@ setTimeout(eliminateNumbers,5000)
 setTimeout(function() {
     eliminateNumbers() // runs first
     askNumbers() // runs second
-  }, 5000)
+    checkPoints() //runs third
+    givePositionRightNumbers() //runs fourth
+}, 5000)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -31,11 +61,70 @@ function eliminateNumbers() {
 function askNumbers () {
 
     for ( i = 0 ; i < elGameNumbers.length ; i++ ) {
-       let userNumber = prompt("inserisci uno alla vota i numeri che hai visto")
-       UserNumbers.push(userNumber)
+       let userNumber = parseInt(prompt("inserisci uno alla vota i numeri che hai visto")) 
+       userNumbers.push(userNumber)
 
     }
 
-    console.log(UserNumbers)
+    console.log(userNumbers)
 
 }
+
+function checkPoints() {
+        for ( i = 0 ; i < gameNumbers.length; i++ ) {
+       
+            if (gameNumbers[i]=== userNumbers[i]) {
+            points = points +1
+        }
+        
+    }
+    console.log(`il tuo punteggio è ${points}`)
+}
+
+function givePositionRightNumbers() {
+   
+    for ( i = 0 ; i < gameNumbers.length; i++ ) {
+       
+        if (gameNumbers[i] === userNumbers[i]) {
+        positionRightNumbers.push(i+1)
+        }
+    
+    }
+    console.log(positionRightNumbers)
+    console.log(`hai indovinato i numeri in posizione ${positionRightNumbers}`)
+}
+
+
+
+
+
+
+
+
+
+
+// crea un array di numeri random che vanno da min a max
+function createNumRandomOrderArr (min, max) {
+
+    // creiamo un array interno alla funzione
+    const intArr = [];
+
+    // ora dobbiamo popolarlo 
+    // finchè la lungheza dell'array è minore al "max" selezionato
+    while (intArr.length < max) {
+        // crea numero random tra min e numero quadrati
+        const nuovoNum = RandomNumMinMax(min, 100);
+        // se il numero è presente nell'array non fare nulla
+        if(intArr.includes(nuovoNum)){
+        // se il numero NON è presente nell'array pushalo dentro 
+        } else {
+        intArr.push(nuovoNum);
+        }
+    }
+    return intArr;
+}
+
+// crea numero random che vada da numMin a numMax
+function RandomNumMinMax (numMin, numMax) {
+    return Math.floor(Math.random() * (numMax - numMin + 1) + numMin)
+ }
