f7fec56890ad788a2b261cf4484e96bf7a3d9da0
Antonio <antonioboksic1603@gmail.com>
2023-05-11 17:36:57 +0200

    problema: prima esegue prompt poi nasconde i numeri

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+
+// crea un array di elementi che si riferiscono ai 5 numeri
 let elNumbers = document.querySelectorAll(".numero")
 console.log(elNumbers)
 
+// dopo 5000ms attiva funzione eliminateNumbers e poi askNumbers
 setTimeout(eliminateNumbers,5000)
 
-
+setTimeout(function() {
+    eliminateNumbers() // runs first
+    askNumbers() // runs second
+  }, 5000)
 
 
 
@@ -17,4 +23,13 @@ function eliminateNumbers() {
         elNumbers[i].innerHTML = "?"
     }
     
+}
+
+function askNumbers () {
+
+    for ( i = 0 ; i < elNumbers.length ; i++ ) {
+        prompt("inserisci uno alla vota i numeri che hai visto")
+
+    }
+
 }
